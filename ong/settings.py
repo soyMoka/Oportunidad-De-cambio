12cfc414e93e267a59e568f5782bcde3d0bf3afc
JunamaRB <tckr.22@gmail.com>
2022-07-23 12:10:09 -0300

    FIN

--- ong/settings.py
+++ ong/settings.py
@@ -52,7 +52,7 @@ ROOT_URLCONF = 'ong.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -102,6 +102,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIR = [
+    BASE_DIR / 'static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- ong/urls.py
+++ ong/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls.static import static
+
+from .views import Index
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', Index, name='Index')
 ]
