85e8165f82cffde87c90e0a2e6f6192d24f3d3b9
gerardo serrano <serranogallegogerardowork@gmail.com>
2022-08-04 17:39:15 -0300

    modelado de funcionalidades, y configuraciones...

--- apps/noticias/models.py
+++ apps/noticias/models.py
@@ -1,16 +1,22 @@
 from django.db import models
 
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=250, null=False)
+
+    def __str__(self):
+        return self.nombre
+
 # Create your models here.
-class Noticia(ModeloBase):
-    ID = ('ID',max_length = 99999)
-    titulo = models.CharField('Título del Post',max_length = 150, unique = True)
-    fecha = models.DateField('fecha')
-    texto = RichTextField()
-    categoria = models.ForeignKey(Categoria, on_delete = models.CASCADE)
+class Noticia(models.Model):
+    #ID = ('ID',max_length = 99999)
+    titulo = models.CharField(max_length=250, null=False)
+    fecha = models.DateTimeField(auto_now_add=True)
+    texto = models.TextField(null=True)
+    categoria = models.ForeignKey(Categoria, on_delete=models.SET_NULL, null=True)
     
-    class Meta:
-        verbose_name = 'Noticia'
-        verbose_name_plural = 'Noticias'
+    #class Meta:
+    #    verbose_name = 'Noticia'
+    #    verbose_name_plural = 'Noticias'
 
     def __str__(self):
         return self.titulo
--- ong/settings.py
+++ ong/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'ong',
-    #'apps.noticias',
+    'apps.noticias',
     #'apps.comentarios',
     #'apps.usuarios',
 ]
@@ -133,6 +133,16 @@ USE_TZ = True
 
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+MEDIA_URL = "/media/"
+
+MEDIA_ROOT = BASE_DIR / "media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
