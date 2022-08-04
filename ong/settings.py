3f86ef8c582d1c1ec740bde17e932969c1cd7e22
gerardo serrano <serranogallegogerardowork@gmail.com>
2022-08-04 16:11:59 -0300

    modificacion en settings

--- ong/settings.py
+++ ong/settings.py
@@ -81,15 +81,6 @@ WSGI_APPLICATION = 'ong.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-''' #ORIGINAL
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-'''
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
