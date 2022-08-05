9f59bc98536149932e42379b407ab7c7571f8b66
serranogallegogerardo <serranogallegogerardowork@gmaill.com>
2022-08-05 18:27:58 -0300

    implementando los comentarios, noticias...

--- apps/comentarios/admin.py
+++ apps/comentarios/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from apps.comentarios.models import Comentario
+from apps.usuarios.models import Usuario
 # Register your models here.
+
+admin.site.register(Comentario)
+admin.site.register(Usuario)
--- apps/comentarios/models.py
+++ apps/comentarios/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from apps.noticias.models   import Noticia
+from apps.noticias.models import Noticia
 from apps.usuarios.models import Usuario
 #from apps.usuarios.models import Usuario
 # Create your models here.
@@ -10,6 +10,6 @@ class Comentario(models.Model):
     noticia = models.ForeignKey(Noticia, on_delete=models.CASCADE)
     usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
     texto = models.TextField(max_length=200)
-    fecha = models.DateTimeField(auto_created=True)
-    
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)    
 
