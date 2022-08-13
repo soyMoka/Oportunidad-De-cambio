f9dfe260ac6c79fb6dd412406636e77b6bc3287f
JuanmaRB <tckr.22@gmail.com>
2022-08-13 19:45:33 -0300

    Cambios en noticia, ojo con el user y pass de mysql en settings

--- apps/noticias/urls.py
+++ apps/noticias/urls.py
@@ -6,6 +6,7 @@ app_name = 'apps.noticia'
 urlpatterns = [
     path('agregar/', views.agregar.as_view()),
     path('mostrar/', views.mostrar.as_view()),
+    path('', views.mostrar.as_view()),
     path('mostrarImg/', views.mostrarImg),
     path('mostrarImg/<str:categoria>', views.mostrarImgCat, name="mostrarCategoria"),
 ]
