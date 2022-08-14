92dfb337d7bc568ae5e8736f7a796fdb1a476616
gerardo serrano <serranogallegogerardowork@gmail.com>
2022-08-14 10:21:18 -0300

    funcionalidad usuario terminada, falta front

--- apps/usuarios/urls.py
+++ apps/usuarios/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
-from .views import RegistrarUsuarios
-
+from .views import RegistrarUsuarios, ModificarUsuario, BorrarUsuario
 #from .views import
 app_name = 'apps.usuario'
 
 urlpatterns = [
     path('login/', LoginView.as_view(template_name="usuario/login.html"),name="login"),
     path('logout/', LogoutView.as_view(),name="logout"),
+    
     path('agregar/', RegistrarUsuarios.as_view() ,name="agregar"),
-    # path('modificarUsuario/<str:pk>', ModificarUsuario.as_view() ,name="modificarUsuario"),
-    # path('eliminarUsuario/<int:pk>', DeleteUsuario.as_view() ,name="eliminarUsuario")
+    path('modificar/<str:pk>', ModificarUsuario.as_view() ,name="modificar"),
+    path('borrar/<int:pk>', BorrarUsuario.as_view() ,name="eliminar")
 ]
